8e3378285443d4dd8d2cbca2a0afa47ecbea338e
Maciej Małecki <me@mmalecki.com>
2023-10-26 00:55:07 +0200

    Start working on the linear rail Y axis

--- cad/assembly.py
+++ cad/assembly.py
@@ -1,5 +1,5 @@
 import cadquery as cq
-from vitamins.vslot import vslot
+from vitamins.vslot import vslot, joiningPlate2
 from settings import Settings
 
 frameTopFront = vslot(Settings.xL)
@@ -7,12 +7,20 @@ frameTopBack = vslot(Settings.xL)
 frameTopLeft = vslot(Settings.yL)
 frameTopRight = vslot(Settings.yL)
 
+yAxisMountFront = joiningPlate2()
+
+yAxisProfile = vslot(Settings.yL + 2 * vslot.d)
+
 assembly = (
     cq.Assembly()
         .add(frameTopFront, name="frameTopFront")
         .add(frameTopLeft, name="frameTopLeft")
         .add(frameTopRight, name="frameTopRight")
         .add(frameTopBack, name="frameTopBack")
+
+        .add(yAxisProfile, name="yAxisProfile")
+        .add(yAxisMountFront, name="yAxisMountFront")
+
         .constrain("frameTopFront", frameTopFront.faces(">Z").edges(">Y").val(), "frameTopLeft", frameTopLeft.faces(">Z").edges(">Y").val(), "Point")
         .constrain("frameTopFront", frameTopFront.faces("<Z").edges(">Y").val(), "frameTopRight", frameTopRight.faces(">Z").edges("<Y").val(), "Point")
         .constrain("frameTopFront@faces@>Y", "frameTopLeft@faces@>Z", "Axis")
@@ -22,7 +30,14 @@ assembly = (
         .constrain("frameTopBack", frameTopBack.faces("<Z").edges("<Y").val(), "frameTopRight", frameTopRight.faces("<Z").edges("<Y").val(), "Point")
         .constrain("frameTopBack@faces@>Y", "frameTopLeft@faces@>Z", "Axis")
         .constrain("frameTopBack@faces@>Y", "frameTopRight@faces@>Z", "Axis")
+
+        .constrain("frameTopFront", frameTopFront.faces("<Y").edges(">X").val(), "yAxisMountFront", yAxisMountFront.faces(">Y").edges(">X").val(), "Axis")
+        .constrain("frameTopFront", frameTopFront.faces("<Y").val(), "yAxisMountFront", yAxisMountFront.edges("%Circle").all()[1].val(), "Plane")
+        .constrain("yAxisProfile", yAxisProfile.faces(">Z").val(), "yAxisMountFront", yAxisMountFront.edges("%Circle").all()[3].val(), "Plane")
+        .constrain("yAxisProfile@faces@>Y", "frameTopFront@faces@<X", "Axis")
 )
+
+
 
 assembly.solve()
 
--- cad/vitamins/vslot.py
+++ cad/vitamins/vslot.py
@@ -1,12 +1,22 @@
 import os
-from workplane import Workplane
 import cadquery as cq
 
 PROFILE = cq.importers.importDXF(os.path.join(os.path.dirname(__file__), "vslot-2020_1.dxf")).wires()
-PROFILE_MOCK = Workplane("front").rect(20, 20)
+VSLOT_D = 20
+PROFILE_MOCK = cq.Workplane("front").rect(VSLOT_D, VSLOT_D)
 
 def vslot(length, mock = True):
     return (PROFILE_MOCK if mock else PROFILE).toPending().extrude(length)
 
+vslot.d = VSLOT_D
+
+def joiningPlate2():
+    plate = cq.importers.importStep(os.path.join(os.path.dirname(__file__), "Joining Strip Plate 2 Hole.step"))
+    plate.faces(">Y").tag("mate0")
+    plate.faces("<Y").tag("mate1")
+    # debug(plate.faces(tag="mate0").edges(">X"))
+    return plate
+
 if 'show_object' in globals():
-    show_object(vslot(100), name="vslot100")
+    # show_object(vslot(100), name="vslot100")
+    show_object(joiningPlate2().translate((5, 0, 0)), name="joiningPlate2")
