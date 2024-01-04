b35105a70bc0c80e451675ee56283f6e6680450f
Maciej Małecki <me@mmalecki.com>
2024-01-04 17:54:30 +0100

    Use dataclasses for settings

--- cad/settings.py
+++ cad/settings.py
@@ -1,5 +1,30 @@
+from dataclasses import dataclass
 import cq_queryabolt
 from vitamins.vslot import vslot
+
+_belts = {
+    'GT2': {
+        'thickness': 2,
+        'width': 6
+    }
+}
+
+_idlers = {
+    'GT2-16T': {
+        'diameter': 14,
+        'height': 9
+    }
+}
+
+@dataclass
+class Belt:
+    thickness: float
+    width: float
+
+@dataclass
+class Idler:
+    diameter: float
+    height: float
 
 class Settings:
     tightFit = 0.1
@@ -18,6 +43,7 @@ class Settings:
     frameBoltD = cq_queryabolt.boltData(frameBolt)['diameter']
     frameBoltWallD = frameBoltD + minBoltWallD
 
+    # Thickness of a plate upon which stepper-like forces act.
     stepperPlateT = 5
 
     sacrificialLayers = 0.2
@@ -30,3 +56,7 @@ class Settings:
     bedT = 4
 
     xL = bedX + 2 * vslot.d + 2 * bedXFrameClearance
+
+    belt = Belt(**_belts['GT2'])
+    yIdler = Idler(**_idlers['GT2-16T'])
+
